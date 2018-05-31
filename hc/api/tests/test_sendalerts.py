99d13837e2f9eba2f382c32b5a872f99e616e045
Muhozi Emery <muhozie@gmail.com>
2018-05-31 16:30:38 +0300

    [Chore #157694749] Fix send alerts tests

--- hc/api/tests/test_sendalerts.py
+++ hc/api/tests/test_sendalerts.py
@@ -21,13 +21,12 @@ class SendAlertsTestCase(BaseTestCase):
             check.save()
 
         result = Command().handle_many()
-        assert result, "handle_many should return True"
+        self.assertTrue(result)
 
         handled_names = []
         for args, kwargs in mock.call_args_list:
             handled_names.append(args[0].name)
-
-        assert set(names) == set(handled_names)
+        self.assertEqual(set(names),set(handled_names))
         ### The above assert fails. Make it pass
 
     def test_it_handles_grace_period(self):
@@ -37,6 +36,7 @@ class SendAlertsTestCase(BaseTestCase):
         check.save()
 
         # Expect no exceptions--
-        Command().handle_one(check)
+        res = Command().handle_one(check)
+        self.assertTrue(res) 
 
     ### Assert when Command's handle many that when handle_many should return True
