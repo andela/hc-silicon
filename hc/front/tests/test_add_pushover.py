903dc33cffc66055f6723f1e030f973617933f74
Muhozi Emery <muhozie@gmail.com>
2018-05-31 06:33:59 +0300

    [Bug] Fix identation error in tests

--- hc/front/tests/test_add_pushover.py
+++ hc/front/tests/test_add_pushover.py
@@ -80,16 +80,16 @@ class AddPushoverTestCase(BaseTestCase):
         r = self.client.get("/integrations/add_pushover/?%s" % params)
         assert r.status_code == 403
         def test_validates_priority(self):
-        """
-            Test adding pushover validates priority
-        """
-        self.client.login(username="alice@example.org", password="password")
+            """
+                Test adding pushover validates priority
+            """
+            self.client.login(username="alice@example.org", password="password")
 
-        session = self.client.session
-        session["po_nonce"] = "n"
-        session.save()
+            session = self.client.session
+            session["po_nonce"] = "n"
+            session.save()
 
-        params = "pushover_user_key=a&nonce=n&prio=Invalid"
-        r = self.client.get("/integrations/add_pushover/?%s" % params)
-        assert r.status_code == 400
+            params = "pushover_user_key=a&nonce=n&prio=Invalid"
+            r = self.client.get("/integrations/add_pushover/?%s" % params)
+            assert r.status_code == 400
     ### Test that pushover validates priority
